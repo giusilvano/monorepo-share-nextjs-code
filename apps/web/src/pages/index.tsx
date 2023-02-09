36d4856ca18fb339c5455ced03c8aed9c88dbf32
Giuseppe Silvano <giuseppe.silvano@gmail.com>
2023-02-09 12:02:31 +0000

    Initial commit

---
+++ apps/web/src/pages/index.tsx
@@ -0,0 +1,5 @@
+import Component1 from "@acme/ui/src/components/Component1";
+
+export default function Home() {
+  return <Component1 />;
+}
---
+++ packages/ui/src/components/Component1.tsx
@@ -0,0 +1,10 @@
+import Component2 from "@/components/Component2";
+
+export default function Component1() {
+  return (
+    <>
+      <div>Component1</div>
+      <Component2 />
+    </>
+  );
+}
